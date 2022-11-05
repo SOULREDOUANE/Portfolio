fb8ad989d34a2d02c8a45082be4614ccb6cf3f99
redouanesoul <redwanesol2000@gmail.com>
2022-11-05 08:54:56 +0100

    this is for the  case i want to use it for react

---
+++ formulaire/formulaire.js
@@ -0,0 +1,17 @@
+// function  language(){
+//     const div_container=document.querySelector("language");
+//     // console.log(div_container);
+//     const div1=document.createElement('div');
+//     div1.classList.add('style_input');
+//     console.log(div1);
+//     div_container.append(div1);
+// }
+function add_language(){
+    const div_container=document.querySelector("language");
+    const div1=document.createElement('div');
+    div1.classList.add('style_input');
+    div_container.append(div1);
+}
+
+
+let x=document.getElementById()
